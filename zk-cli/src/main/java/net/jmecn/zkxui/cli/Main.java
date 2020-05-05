7c7acb24d5071eed1864caef86df3f59f8544158
jmecn <115050813@qq.com>
2020-05-05 16:39:18 +0800

    [A]Add a entry for both gui and tui client.

---
+++ zk-cli/src/main/java/net/jmecn/zkxui/cli/Main.java
@@ -0,0 +1,19 @@
+package net.jmecn.zkxui.cli;
+
+import lombok.extern.slf4j.Slf4j;
+import net.jmecn.zkxui.gui.ZkGuiMain;
+import net.jmecn.zkxui.tui.ZkTuiMain;
+
+@Slf4j
+public class Main {
+
+	public static void main(String[] args) {
+		log.info("ZK-XUI started.");
+		if (java.awt.GraphicsEnvironment.isHeadless()) {
+			ZkTuiMain.main(args);
+		} else {
+			ZkGuiMain.main(args);
+		}
+	}
+
+}
